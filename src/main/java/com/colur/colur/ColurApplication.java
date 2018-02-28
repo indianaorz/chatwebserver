bb8c2c88f53f7e8f1b94dc2cf711a427c8d11815
lee <leeeorzol@gmail.com>
2018-02-28 17:21:06 -0600

    restarted project to use JPA Repos

---
+++ src/main/java/com/colur/colur/ColurApplication.java
@@ -0,0 +1,12 @@
+package com.colur.colur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColurApplication.class, args);
+	}
+}
