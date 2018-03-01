0e3078ae97cf9f5557f99d030c3089d1257da8cb
lee <leeeorzol@gmail.com>
2018-03-01 17:29:09 -0600

    added CORS

--- src/main/java/com/colur/rest/color/ColorRestController.java
+++ src/main/java/com/colur/rest/color/ColorRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.net.URI;
 import java.util.Collection;
@@ -27,6 +28,7 @@ class ColorRestController {
 		this.colorRepository = colorRepository;
 	}
 
+  	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET)
 	Collection<Color> getColors(@PathVariable String userId) {
 		this.validateUser(userId);
